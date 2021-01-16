b024f912d34829526504e2c07f90b0c5e8fa9151
tomasjakub <tomasjakub21@gmail.com>
2021-01-16 13:40:36 +0100

    Initial commit

---
+++ src/main/java/com.learn/Main.java
@@ -0,0 +1,13 @@
+package com.learn;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello world");
+
+        
+    }
+}
