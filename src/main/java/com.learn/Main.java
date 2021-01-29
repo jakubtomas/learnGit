15ebd93e85a8079004ef1541c4bf9b8c94bcbca3
tomasjakub <tomasjakub21@gmail.com>
2021-01-29 14:51:34 +0100

    "add line of code22 "

--- src/main/java/com.learn/Main.java
+++ src/main/java/com.learn/Main.java
@@ -11,6 +11,7 @@ public class Main {
         System.out.println("second commit");
 
         System.out.println("3 comit ");
+        System.out.println("hello ");
 
 
         System.out.println("Hello next commit ");
