72504f48d714ff23576eb6e903789bfc20867e78
dolinsky <martin.dolinsky@akademiasovy.sk>
2021-02-03 10:58:37 +0100

    Changed Jakub

--- src/main/java/com.learn/Main.java
+++ src/main/java/com.learn/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("hello Kubo");
+        System.out.println("hello Jakub");
 
         System.out.println("second commit");
 
