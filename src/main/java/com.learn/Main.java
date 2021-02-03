5ce6d4fd98d4f3ea101b6ce57bf31ce3c3ce0f44
jakubvasinsky <jakub.vasinsky@gmail.com>
2021-02-03 10:58:07 +0100

    commit

--- src/main/java/com.learn/Main.java
+++ src/main/java/com.learn/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("hello Kubo");
+        System.out.println("Kubo Hello");
 
         System.out.println("second commit");
 
