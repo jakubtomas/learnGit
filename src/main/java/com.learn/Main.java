79bc6379c75ebd5a03ae124e914de9ec045d63cf
dolinsky <martin.dolinsky@akademiasovy.sk>
2021-02-03 11:24:57 +0100

    dolinsky branch commit 1

--- src/main/java/com.learn/Main.java
+++ src/main/java/com.learn/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello Miro");
+        System.out.println("Dolinsky branch");
 
 
         System.out.println("second commit");
