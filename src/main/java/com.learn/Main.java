3ca6ef019bc41fa83595ca5409c221ecc24c2146
dolinsky <martin.dolinsky@akademiasovy.sk>
2021-02-03 11:43:22 +0100

    please work

--- src/main/java/com.learn/Main.java
+++ src/main/java/com.learn/Main.java
@@ -8,6 +8,10 @@ public class Main {
 
         System.out.println("Dolinsky branch ");
 
+		System.out.println("fero");
+		System.out.println("fero");
+		System.out.println("fero");
+		System.out.println("fero");
 
         System.out.println("second commit");
 
